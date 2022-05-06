44d6f7b0c44a93ed549c063a3863e36dd3dda0e1
ohhyeonmyeong <101865438+ohhyeonmyeong@users.noreply.github.com>
2022-05-06 17:24:34 +0900

    First

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $(".intro").click(function(){
+
+        $("html, body").animate({scrollTop : $("#cont1 article:first-child").offset().top - 80}, 1000);
+        $("#open_menu").prop("checked", false) // prop() : [property -> 속성] 내부의 변경할 값을 강제로 넣는다
+        return false;
+    });
+
+    $(".story").click(function(){
+
+        $("html, body").animate({scrollTop : $("#cont1 article:last-child").offset().top - 80}, 1000);
+        $("#open_menu").prop("checked", false)
+        return false;
+    });
+
+    $(".journal").click(function(){
+
+        $("html, body").animate({scrollTop : $("#cont2 article:first-child").offset().top - 80}, 1000);
+        $("#open_menu").prop("checked", false)
+        return false;
+    });
+
+    $(".stores").click(function(){
+
+        $("html, body").animate({scrollTop : $("#cont2 article:last-child").offset().top - 80}, 1000);
+        $("#open_menu").prop("checked", false)
+        return false;
+    });
+
+});
